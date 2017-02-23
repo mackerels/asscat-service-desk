77bb4c54b7c90d64eb64ed430ae336dd3ffb39c4
Paul <b.paul4@gmail.com>
2017-02-23 19:43:52 +0200

    Необходимо добавить название вопроса и его описание.

--- SD/Models/IssueModel.cs
+++ SD/Models/IssueModel.cs
@@ -7,7 +7,12 @@ namespace SD.Models
     {
         public int Id { get; set; }
 
+        public string Name{get; set;}
+
+        public string Memo { get; set; }
+
         public IssueStatus Status { get; set; }
+
         public IEnumerable<CommentModel> Comments { get; set; }
 
         public UserModel Issuer { get; set; }
