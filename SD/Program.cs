24fed685a8cd192b8a03f37b8b7f08ff3994ade8
421p <andrew.bakal@gmail.com>
2017-02-23 12:02:06 +0200

    initial

---
+++ SD/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace service_desk
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Path.Combine(Directory.GetCurrentDirectory(), "frontend"))
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:22022")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ SD/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace service_desk
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddCors()
+                .AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app
+                .UseDeveloperExceptionPage()
+                .UseDefaultFiles()
+                .UseStaticFiles()
+                .UseMvc()
+                .UseCors(builder => builder.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin());
+        }
+    }
+}
