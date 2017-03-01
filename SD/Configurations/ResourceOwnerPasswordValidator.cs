1aa19526d00d602fdef81f4cbd700ddde8ee87b9
421p <andrew.bakal@gmail.com>
2017-03-01 12:31:36 +0200

    add identityserver4, creating tokens

---
+++ SD/Configurations/ResourceOwnerPasswordValidator.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Validation;
+
+namespace SD.Configurations
+{
+    public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var login = context.UserName;
+            var pass = context.Password;
+
+            if (login == "test" && pass == "test")
+            {
+                context.Result = new GrantValidationResult(
+                    "test_id", "password"
+                );
+            }
+            else
+            {
+                context.Result = new GrantValidationResult(
+                    TokenRequestErrors.InvalidRequest,
+                    "login or password is incorrect"
+                );
+            }
+
+            return Task.FromResult(0);
+        }
+    }
+}
--- SD/Startup.cs
+++ SD/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using System.Reflection;
+using IdentityServer4.Services;
+using IdentityServer4.Validation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SD.Configurations;
 
 namespace SD
 {
@@ -13,6 +16,13 @@ namespace SD
             services
                 .AddCors()
                 .AddMvc();
+            services.AddIdentityServer()
+                .AddTemporarySigningCredential()
+                .AddInMemoryApiResources(ApiResources.GetApiResources())
+                .AddInMemoryClients(Clients.GetClients());
+
+            services.AddTransient<IResourceOwnerPasswordValidator, ResourceOwnerPasswordValidator>();
+            services.AddTransient<IProfileService, ProfileService>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -26,7 +36,8 @@ namespace SD
                 .UseMvc()
                 .UseCors(builder => builder.AllowAnyHeader()
                     .AllowAnyMethod()
-                    .AllowAnyOrigin());
+                    .AllowAnyOrigin())
+                .UseIdentityServer();
         }
     }
 }
